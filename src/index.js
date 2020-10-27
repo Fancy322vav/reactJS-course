78785b1628de1143654bd0e2b765c08c91edb8de
Aliaksei Vysotsky <alexeyvysotsky97@gmail.com>
2020-10-27 23:07:04 +0300

    Fixing routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Layout from "./components/Layout";
+import { Switch, Route } from "react-router-dom";
+import SignIn from "./containers/SignInData";
+import App from "./containers/App";
+import Header from "./components/Header";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
+import CardContextProvider from "./context/CardContext";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Layout />
+    <CardContextProvider>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route
+          render={() => <h1 style={{ textAlign: "center" }}>Page not found</h1>}
+        />
+      </Switch>
+    </CardContextProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
